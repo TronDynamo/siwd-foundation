209ce42b46942415f8e57092299d59ffdf16e8b8
TronDynamo <godzilla9zjb@gmail.com>
2026-08-18 19:08:50 -0400

    fix tailwind - restore design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,12 @@ import type { Config } from 'tailwindcss';
  */
 const config: Config = {
   darkMode: ['class'],
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}']
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     container: { center: true, padding: '1rem', screens: { '2xl': '1400px' } },
     extend: {

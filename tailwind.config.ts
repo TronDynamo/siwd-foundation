080e067a3aec700559735ad4aec97da38a172bab
TronDynamo <godzilla9zjb@gmail.com>
2026-08-18 18:48:17 -0400

    fix tailwind content paths - restore design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ import type { Config } from 'tailwindcss';
  */
 const config: Config = {
   darkMode: ['class'],
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}']
   theme: {
     container: { center: true, padding: '1rem', screens: { '2xl': '1400px' } },
     extend: {

dba2fe55561c8645907b9292b4821fde558fb1fc
TronDynamo <godzilla9zjb@gmail.com>
2026-08-18 20:29:17 -0400

    rewrite configs for tailwind v4 fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,28 @@
 import type { Config } from "tailwindcss"
+
 const config: Config = {
-  darkMode: ["class"],
-  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       keyframes: {
         "accordion-down": { from: { height: "0" }, to: { height: "var(--radix-accordion-content-height)" } },
         "accordion-up": { from: { height: "var(--radix-accordion-content-height)" }, to: { height: "0" } },
-        "egg-spin": { "0%": { transform: "rotate(0deg)" }, "100%": { transform: "rotate(360deg)" } }
+        "egg-spin": { "0%": { transform: "rotate(0deg)" }, "100%": { transform: "rotate(360deg)" } },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "egg-spin": "egg-spin 0.7s cubic-bezier(0.4,0,0.2,1) 1"
-      }
-    }
+        "egg-spin": "egg-spin 0.7s cubic-bezier(0.4,0,0.2,1) 1",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 }
+
 export default config
